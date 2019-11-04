6776fc3038101272199d05d807071060ef5eb20e
catalin98 <56113419+catalin98@users.noreply.github.com>
2019-11-04 10:48:16 +0200

    Add files via upload

--- HTML/gyro/gyro.js
+++ HTML/gyro/gyro.js
@@ -1,10 +1,10 @@
-document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.0";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.1";
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 function on_gyro_data_uab(e)
 {
-	document.getElementById("id_alpha").innerHTML ="Logic id_alpha";
-	document.getElementById("id_beta").innerHTML="Logic id_beta";
-	document.getElementById("id_gamma")innerHTML="Logic id_gamma";
+	document.getElementById("id_alpha").innerHTML =(e.alpha * 100)/100;
+	document.getElementById("id_beta").innerHTML=(e.beta * 100)/100;
+	document.getElementById("id_gamma")innerHTML=(e.gamma * 100)/100;
 	
 	
 	

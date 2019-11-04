4666a0a449e9000e92dc57766104b0773e5d9fd3
catalin98 <56113419+catalin98@users.noreply.github.com>
2019-11-04 11:49:19 +0200

    Add files via upload

--- HTML/gyro/gyro.js
+++ HTML/gyro/gyro.js
@@ -13,8 +13,8 @@ function on_gyro_data_uab(e)
 function on_acc_data_uab(e)
 {   var acc = e.accelerationIncludingGravity;
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc.x *100 )/100
-	document.getElementById("id_acc_x").innerHTML = Math.round(acc.y *100 )/100
-	document.getElementById("id_acc_x").innerHTML = Math.round(acc.z *100 )/100
+	document.getElementById("id_acc_y").innerHTML = Math.round(acc.y *100 )/100
+	document.getElementById("id_acc_z").innerHTML = Math.round(acc.z *100 )/100
 	
 }
 

2291ed298a89775cb9596f597a713a565cd5e4ff
catalin98 <56113419+catalin98@users.noreply.github.com>
2019-11-04 12:08:36 +0200

    Add files via upload

--- HTML/gyro/gyro.js
+++ HTML/gyro/gyro.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.1";
+document.getElementById("id_logic_version").innerHTML="Logic = 2019.11.04.2";
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotiontion", on_acc_data_uab);
 function on_gyro_data_uab(e)
@@ -15,6 +15,13 @@ function on_acc_data_uab(e)
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc.x *100 )/100
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc.y *100 )/100
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc.z *100 )/100
+	
+	var rot_x = Math.atan(acc.x/ acc.z) * 180 / Math.PI;
+	var rot_x = Math.atan(acc.y/ acc.z) * 180 / Math.PI;
+	
+	document.getElementById("id_rot_x").innerHTML = Math.round(rot_x *100)/100;
+	document.getElementById("id_rot_y").innerHTML = Math.round(rot_x *100)/100;
+	document.getElementById("id_rot_z").innerHTML = Math.round(rot_x *100)/100;
 	
 }
 
